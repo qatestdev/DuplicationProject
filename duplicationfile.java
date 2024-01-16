5fe27eebc19c8f5653cc258227b08f6dd78612a3
devcodacytestuser <devcodacytestuser@codacy.com>
2024-01-16 16:10:53 +0000

    Update duplicationfile.java

--- duplicationfile.java
+++ duplicationfile.java
@@ -50,6 +50,27 @@ public void tc03setUp() {
         homepage.landingPage();
         authentication.login(LoginMethod.BITBUCKET, testEnvironment.email(), testEnvironment.password(), Optional.empty());
     }
+  public void tc03setUp() {
+        Authentication authentication = new Authentication(driver, testEnvironment);
+        Homepage homepage = new Homepage(driver, testEnvironment);
+
+        homepage.landingPage();
+        authentication.login(LoginMethod.BITBUCKET, testEnvironment.email(), testEnvironment.password(), Optional.empty());
+    }
+  public void tc03setUp() {
+        Authentication authentication = new Authentication(driver, testEnvironment);
+        Homepage homepage = new Homepage(driver, testEnvironment);
+
+        homepage.landingPage();
+        authentication.login(LoginMethod.BITBUCKET, testEnvironment.email(), testEnvironment.password(), Optional.empty());
+    }
+  public void tc03setUp() {
+        Authentication authentication = new Authentication(driver, testEnvironment);
+        Homepage homepage = new Homepage(driver, testEnvironment);
+
+        homepage.landingPage();
+        authentication.login(LoginMethod.BITBUCKET, testEnvironment.email(), testEnvironment.password(), Optional.empty());
+    }
    public void tc03setUp() {
         Authentication authentication = new Authentication(driver, testEnvironment);
         Homepage homepage = new Homepage(driver, testEnvironment);
